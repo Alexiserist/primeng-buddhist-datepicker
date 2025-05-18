32b3f2cb9143f3dbd963d03b5c75ac4093afc9ee
Chananuwat Chaysri <asiavip2546@gmail.com>
2025-05-18 18:11:39 +0700

    first commit

---
+++ projects/demo-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DatePickerModule } from 'primeng-buddhist-datepicker';
+
+@Component({
+  selector: 'app-root',
+  imports: [DatePickerModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'demo-app';
+
+  date:any;
+  dateRange: any;
+  
+
+  logData(){
+    console.log(this.dateRange)
+  }
+}
